ffc56c04d4ef87123390f4a1491f1715c66e0055
AdityaKakkad <74364910+AdityaKakkad@users.noreply.github.com>
2021-07-10 19:11:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(640,480);
+    video.hide();
+  }
+
+  function draw() {
+    image(video,100,20,450,450);
+
+    fill(0, 0, 455);
+    stroke(0, 450, 0);
+    rect(90, 10, 460, 20);
+    rect(90, 450, 460, 20);
+    rect(90, 10, 20, 460);
+    rect(550, 10, 20, 460);
+  }
+
+  function saveImage(){
+    save("digital_frame(crop-me).png");
+  }
